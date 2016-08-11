f71396b0238a990810a9a0ed8ced462e03728764
Jakub Kwasek <jakub.kwasek@gmail.com>
2016-08-11 20:34:56 +0200

    add skeleton boilerplate

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('dist/assets'));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/app/components/');
 
